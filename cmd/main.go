6f97eca60ca6e9316c38b94690d1a4611e14b0ab
cbergz <charlie.bergman@gmail.com>
2023-11-06 21:17:25 -0500

    add code for dune upload and change to csv file

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	bridge "dydx-bridge-monitor/pkg/contracts"
+	"encoding/csv"
 	"log"
 	"math/big"
+	"os"
+	"strconv"
 
 	"github.com/cosmos/cosmos-sdk/types/bech32"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -11,17 +14,16 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	_ "github.com/jackc/pgx/stdlib"
 	_ "github.com/lib/pq"
-	"gopkg.in/DataDog/dd-trace-go.v1/contrib/jmoiron/sqlx"
 )
 
 func main() {
 	// Setup the database connection and insert command
-	db, err := sqlx.Connect("pgx", "postgresql://doadmin:AVNS_A3mgPzwuR6swL6WYcPs@db-postgresql-nyc1-02376-do-user-3592284-0.b.db.ondigitalocean.com:25060/dydx?sslmode=require")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-	insertStmt := `INSERT INTO bridge_events (id, sender, receiver, amount) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO NOTHING`
+	// db, err := sqlx.Connect("pgx", "postgresql://doadmin:AVNS_A3mgPzwuR6swL6WYcPs@db-postgresql-nyc1-02376-do-user-3592284-0.b.db.ondigitalocean.com:25060/dydx?sslmode=require")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer db.Close()
+	// insertStmt := `INSERT INTO bridge_events (id, sender, receiver, amount) VALUES ($1, $2, $3, $4) ON CONFLICT (id) DO NOTHING`
 
 	// Setup the connection to Ethereum using Infura RPC
 	client, err := ethclient.Dial("https://mainnet.infura.io/v3/f61acb8dac8b4fa5bd201f129cc37a67")
@@ -43,21 +45,62 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Open or create a CSV file for reading and writing
+	csvFilePath := "bridge_events.csv"
+	csvFile, err := os.OpenFile(csvFilePath, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvFile.Close()
+
+	// Read existing data to avoid duplicates
+	existingData := make(map[string]bool)
+	csvReader := csv.NewReader(csvFile)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, record := range records {
+		// Assuming the event ID is in the first column
+		existingData[record[0]] = true
+	}
+
+	// Create a CSV writer
+	csvWriter := csv.NewWriter(csvFile)
+	csvWriter.Comma = ',' // Set the CSV delimiter
+
 	// Loop through all the events to find any new bridge transfers
 	for bridgeTransfers.Next() {
 		event := bridgeTransfers.Event
 
 		// Convert the values as needed for reading and inserting
-		eventId := event.Id.Int64()
+		eventID := event.Id.Int64()
+
+		// Check for duplicates based on event ID
+		if existingData[strconv.FormatInt(eventID, 10)] {
+			continue // Skip duplicates
+		}
+
 		dydxAmount, _ := new(big.Rat).SetInt(event.Amount).Float64()
 		dydxAmount = dydxAmount / float64(10e17)
 		cosmosAddress, _ := bech32.ConvertAndEncode("dydx", event.AccAddress)
 
-		// Add new transfers to the database
-		_, err := db.Exec(insertStmt, eventId, event.From.String(), cosmosAddress, dydxAmount)
+		// Add new transfers to the CSV file
+		data := []string{
+			strconv.FormatInt(eventID, 10),
+			event.From.String(),
+			cosmosAddress,
+			strconv.FormatFloat(dydxAmount, 'f', -1, 64),
+		}
+		err = csvWriter.Write(data)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
 
+	// Flush any buffered data to the CSV file
+	csvWriter.Flush()
+	if err := csvWriter.Error(); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ pkg/dune/dune.go
@@ -0,0 +1,72 @@
+package dune
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func uploadToDune() {
+	apiKey := "azqtH0LzxiCssnnL86uSkjdhDixni70r"
+	csvFilePath := "/home/cbergz/bridge_events.csv"
+	url := "https://api.dune.com/api/v1/table/upload/csv"
+
+	file, err := os.Open(csvFilePath)
+	if err != nil {
+		fmt.Println("Error opening CSV file:", err)
+		return
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Error reading CSV file:", err)
+		return
+	}
+
+	headers := map[string]string{
+		"X-Dune-Api-Key": apiKey,
+	}
+
+	payload := map[string]interface{}{
+		"table_name":  "dydx_bridge_events",
+		"description": "dYdX bridge events mapped to bech32 addresses",
+		"is_private":  false,
+		"data":        string(data),
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println("Error encoding JSON payload:", err)
+		return
+	}
+
+	response, err := sendPostRequest(url, headers, jsonPayload)
+	if err != nil {
+		fmt.Println("Error sending POST request:", err)
+		return
+	}
+
+	fmt.Println("Response status code:", response.Status)
+	responseData, _ := io.ReadAll(response.Body)
+	fmt.Println("Response content:", string(responseData))
+}
+
+func sendPostRequest(url string, headers map[string]string, data []byte) (*http.Response, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+
+	return client.Do(req)
+}
